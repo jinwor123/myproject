397203a6edf477eb8b230ecdb4294f55f360eb3d
jinwor123 <158055647+jinwor123@users.noreply.github.com>
2026-02-24 18:08:10 +0700

    Add files via upload

---
+++ A project/inventoryproject/dashboard/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Product,Order
+from django.contrib.auth.models import Group
+# Register your models here.
+admin.site.site_header = 'FoxInventory Dashborad'
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('name','category','quantity')
+    list_filter = ['category']
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
+#admin.site.unregister(Group)
