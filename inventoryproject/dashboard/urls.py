069c79650d0e3a102faf4ea566e506dc6ce7e9a0
jinwor123 <158055647+jinwor123@users.noreply.github.com>
2026-03-17 17:59:28 +0700

    Add files via upload

---
+++ inventoryproject/dashboard/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index,name='dashboard-index'),
+    path('staff/',views.staff,name='dashboard-staff'),
+    path('product/', views.product, name='dashboard-product'),
+    #path('product/add/', views.add_product, name='dashboard-add_product'),
+    path('product/category/', views.add_category, name='dashboard-category'),
+    path('category/<int:pk>/delete/', views.delete_category, name='delete-category'),
+    path('product/unit/', views.add_unit, name='dashboard-unit'),
+    path('unit/<int:pk>/delete/', views.delete_unit, name='delete-unit'),
+    path('order/',views.order,name='dashboard-order'),
+    
+]
