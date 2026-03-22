2825b9ef92628ae1791839053d818a4103e4d938
jinwor123 <158055647+jinwor123@users.noreply.github.com>
2026-03-23 00:04:47 +0700

    Add files via upload

--- A project/inventoryproject/inventoryproject/settings.py
+++ A project/inventoryproject/inventoryproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dashboard.apps.DashboardConfig',
     'user.apps.UserConfig',
+    'pos.app.PosConfig',
     'crispy_forms',
     'crispy_bootstrap4'
 ]
